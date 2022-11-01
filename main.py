12865fc532a079cf1385d50be2d9764407438702
vortex2jm <joaopaulomoura5328@gmail.com>
2022-11-01 13:43:27 -0300

    :recycle: Adding email function

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 from subprocess import Popen, PIPE, STDOUT
 from event import Event
 import json
+import smtplib
+import email.message
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SENDER = os.getenv("SENDER")
+RECEIVER = os.getenv("RECEIVER")
+APP_PASSWORD = os.getenv("APP_PASSWORD")
+SUBJECT = 'PET Virtual Machine Services'
+
+def send_email(body):
+    msg = email.message.Message()
+    msg['Subject'] = SUBJECT
+    msg['From'] = SENDER
+    msg['To'] = RECEIVER
+    password = APP_PASSWORD 
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(body)
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    # Login Credentials for sending the mail
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print(f'Email enviado para {RECEIVER}')
 
 def dispatcher(event_json):
     event = Event(event_json)
@@ -16,7 +43,6 @@ def main():
         for line in iter(process.stdout.readline, b''):
             json_string = line.decode("utf-8").strip()
             dispatcher(json.loads(json_string))
-
 
 if __name__ == "__main__":
     main()
